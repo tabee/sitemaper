f16720e9540a5a24a1d8ecb640bcb197a5b1d346
tabee <bee.mario@gmail.com>
2023-08-05 20:34:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sitemaper',
-    version='0.1.8',
+    version='0.1.9',
     packages=find_packages(),
     url='https://github.com/tabee/sitemaper',
     license='MIT License',
